11eaae2a6bb5a650234755fa4f11df2b44e52bec
Aviivix <47121982+Aviivix@users.noreply.github.com>
2022-01-04 15:16:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // v0.2.0
 
-document.getElementById("version_number").innerHTML = "ZSUIMS v0.2.5";
+document.getElementById("version_number").innerHTML = "ZSUIMS v0.2.8";
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
