53dbfcabb06c336d07c0d3039478fe773d03a4ec
Aviivix <47121982+Aviivix@users.noreply.github.com>
2022-01-25 21:15:39 +0000

    Update script.js

--- rw/script.js
+++ rw/script.js
@@ -10,12 +10,14 @@ screenIndex = 0
 cur = document.querySelector('.carousel_container_main')
 nxt = document.querySelector('.carousel_container_next')
 
+
 time=setInterval(async function(){
 	if (screenIndex + 1 == screenshots.length) {
 		screenIndex = 0
 	} else {
 		screenIndex = screenIndex + 1
 	}
+	
 	nxt.style.backgroundImage = 'url(assets/screenshots/' + screenshots[screenIndex] + ')'
 	
 	cur.style.marginLeft = '100%'
@@ -35,4 +37,12 @@ time=setInterval(async function(){
 	cur.style.transition = 'margin 0.5s'
 	nxt.style.transition = 'margin 0.5s'
 	
-},4400);
+	
+	if (screenIndex + 1 == screenshots.length) {
+		document.getElementById('carousel_preload').style.backgroundImage = 'url(assets/screenshots/' + screenshots[0] + ')'
+	} else {
+		document.getElementById('carousel_preload').style.backgroundImage = 'url(assets/screenshots/' + screenshots[screenIndex+1] + ')'
+	}
+	
+	
+},4000);
