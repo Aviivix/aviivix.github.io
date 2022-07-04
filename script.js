772143609b7f41fa4f1323d8ab7e7d3c1893c57a
Aviivix <47121982+Aviivix@users.noreply.github.com>
2022-07-04 17:10:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ function draw() {
 	// Nabbed from http://jsfiddle.net/loktar/63QZz/ https://stackoverflow.com/questions/23468218/draw-10-000-objects-on-canvas-javascript
 	
 	fr += fr;
+	console.log("draw");
 	
 	var canvasData = ctx.createImageData(width, height),
 		cData = canvasData.data;
